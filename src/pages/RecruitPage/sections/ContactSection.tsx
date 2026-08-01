53acab4985c6488cd84fb50ac14e5ce44f49b434
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-08-01 17:27:31 +0900

    feat: 카카오톡 오픈채팅 문의 링크 연결

--- src/pages/RecruitPage/sections/ContactSection.tsx
+++ src/pages/RecruitPage/sections/ContactSection.tsx
@@ -4,9 +4,14 @@ export function ContactSection() {
   return (
     <section className={styles.contactSection}>
       <h2>더 자세한 궁금한 내용이 있다면?</h2>
-      <button className={styles.contactButton} type="button">
+      <a
+        className={styles.contactButton}
+        href="https://open.kakao.com/o/sKOMlKGi"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         카카오톡 오픈채팅 문의하기
-      </button>
+      </a>
     </section>
   );
 }
